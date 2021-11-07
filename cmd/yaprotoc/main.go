650d9861349cfd2004859803a34b88d6014d655a
wdvxdr <wdvxdr1123@gmail.com>
2021-11-07 11:19:24 +0800

    feat: allow concurrent parse proto file

--- cmd/yaprotoc/main.go
+++ cmd/yaprotoc/main.go
@@ -8,7 +8,8 @@ import (
 	"path/filepath"
 	"runtime"
 	"strings"
-	"sync"
+
+	"golang.org/x/sync/errgroup"
 
 	"github.com/wdvxdr1123/yaproto/internal/generator"
 	"github.com/wdvxdr1123/yaproto/internal/importer"
@@ -23,14 +24,14 @@ var (
 
 func main() {
 	flag.StringVar(&importer.ProtoPath, "pkg", "", "package prefix")
-
 	wd, err := os.Getwd()
 	if err != nil {
 		panic(err)
 	}
-
 	flag.Parse()
-	err = filepath.Walk(flag.Args()[0], func(path string, info fs.FileInfo, err error) error {
+
+	group := errgroup.Group{}
+	_ = filepath.Walk(flag.Args()[0], func(path string, info fs.FileInfo, err error) error {
 		if info.IsDir() {
 			return nil
 		}
@@ -38,14 +39,20 @@ func main() {
 		if filepath.Ext(path) != ".proto" {
 			return nil
 		}
-		pkg, err := importer.Import(path)
-		pkg.Error = func(err error) {
-			println(path, err.Error())
-		}
-		return err
+		group.Go(func() error {
+			p, err := importer.Import(path)
+			if err != nil {
+				return err
+			}
+			p.Error = func(err error) {
+				println(err)
+			}
+			return nil
+		})
+		return nil
 	})
 
-	if err != nil {
+	if err := group.Wait(); err != nil {
 		println(err.Error())
 		return
 	}
@@ -54,53 +61,45 @@ func main() {
 	for _, p := range importer.Packages {
 		all = append(all, p)
 	}
-	workers := len(all)
-	if runtime.NumCPU() < workers {
-		workers = runtime.NumCPU()
-	}
 
-	var mu sync.Mutex
-	var wg sync.WaitGroup
-	for i := 0; i < workers; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			var pkg *importer.Package
-			for {
-				mu.Lock()
-				if len(all) == 0 {
-					mu.Unlock()
-					break
-				}
-				pkg = all[0]
-				all = all[1:]
-				mu.Unlock()
+	for _, p := range all {
+		pkg := p
+		group.Go(func() error {
+			pkg.Resolve()
+			g := generator.New(pkg)
+			g.Options.GenGetter = *getter
+			g.Options.GenMarshal = *marshal
+			g.Options.GenSize = *size || *marshal > 1
 
-				pkg.Resolve()
-				g := generator.New(pkg)
-				g.Options.GenGetter = *getter
-				g.Options.GenMarshal = *marshal
-				g.Options.GenSize = *size || *marshal > 1
-
-				if runtime.GOOS == "windows" {
-					wd = strings.Replace(wd, "\\", "/", -1)
-				}
-				outputPath := path.Clean(path.Join(wd, *output, pkg.OutputPath))
-
-				filename := strings.TrimSuffix(pkg.Path, ".proto")
-				dot := strings.LastIndexByte(filename, '/')
-				if dot > 0 {
-					filename = filename[dot+1:]
-				}
-
-				outfile := path.Join(outputPath, filename+".pb.go")
-				_ = os.MkdirAll(path.Dir(outfile), 0644)
-
-				out, _ := os.OpenFile(outfile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
-				g.Generate(out)
-				_ = out.Close()
+			if runtime.GOOS == "windows" {
+				wd = strings.Replace(wd, "\\", "/", -1)
 			}
-		}()
+			outputPath := path.Clean(path.Join(wd, *output, pkg.OutputPath))
+
+			filename := strings.TrimSuffix(pkg.Path, ".proto")
+			dot := strings.LastIndexByte(filename, '/')
+			if dot > 0 {
+				filename = filename[dot+1:]
+			}
+
+			outfile := path.Join(outputPath, filename+".pb.go")
+			err := os.MkdirAll(path.Dir(outfile), 0644)
+			if err != nil {
+				return err
+			}
+
+			out, err := os.OpenFile(outfile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+			if err != nil {
+				return err
+			}
+
+			g.Generate(out)
+			return out.Close()
+		})
 	}
-	wg.Wait()
+	err = group.Wait()
+	if err != nil {
+		println(err.Error())
+		return
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/wdvxdr1123/yaproto
 
 go 1.17
 
-require github.com/emicklei/proto v1.9.1
+require (
+	github.com/emicklei/proto v1.9.1
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
+)
